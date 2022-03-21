eac69c31b942664a79578c18fb06b4ec82a33356
Tim Fuerth <Tim.Fuerth@hakneumarkt.at>
2022-03-21 11:06:53 +0100

    Objekte Klasse

---
+++ Objekte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakegame
+{
+    abstract internal class Objekte
+    {
+        PictureBox Abbildung;
+        int x;
+        int y;
+
+        public Objekte(PictureBox bild, int x, int y)
+        {
+            Abbildung = bild;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
